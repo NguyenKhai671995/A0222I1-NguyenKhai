baeaebcf2cd157a31a8bdf368abb246299b1534c
NguyenKhai671995 <nguyenkhai67.1995@gmail.com>
2022-09-25 17:55:12 +0700

    ss2_exs

--- Module4/MVC/ss1/bai_tap/dictionary/src/main/java/com/codegym/controller/DictionaryController.java
+++ Module4/MVC/ss1/bai_tap/dictionary/src/main/java/com/codegym/controller/DictionaryController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class DictionaryController {
 
      IDicSer iDicSer;
-
      @Autowired
      public DictionaryController (){
          this.iDicSer = new DictionaryService();
